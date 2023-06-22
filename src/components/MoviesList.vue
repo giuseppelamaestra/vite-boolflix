19acabe8280c548c9a5cc87d5c6ec0136181a5c7
giuseppelamaestra <giuseppe.la.maestra@gmail.com>
2023-06-22 11:57:12 +0200

    created new components MoviesList

---
+++ src/components/MoviesList.vue
@@ -0,0 +1,18 @@
+<template>
+  <h2>
+    MoviesList
+  </h2>
+</template>
+<script>
+export default {
+    name: 'MoviesList',
+    props: {
+
+    },
+    methods : {
+
+    },
+}
+</script>
+<style lang="scss">
+</style>
